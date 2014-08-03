b06c8f01ef2683a8ed75e265b306b8a92b6a6f67
martin <martin@zippyrate.com>
2014-08-03 14:52:53 -0700

    Added a Drools class expression resolver

---
+++ src/main/java/org/swrlapi/drools/core/CEResolver.java
@@ -0,0 +1,50 @@
+package org.swrlapi.drools.core;
+
+import org.semanticweb.owlapi.model.*;
+import org.swrlapi.core.OWLClassExpressionResolver;
+import org.swrlapi.drools.owl.classexpressions.*;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class CEResolver
+{
+	private final Map<OWLClassExpression, String> owlClassExpression2CEIDMap;
+	private final Set<String> convertedClassExpressionIDs;
+	private final Set<CE> ces;
+	private int classExpressionIndex;
+
+	public CEResolver()
+	{
+		this.classExpressionIndex = 0;
+		this.ces = new HashSet<CE>();
+		this.owlClassExpression2CEIDMap = new HashMap<OWLClassExpression, String>();
+		this.convertedClassExpressionIDs = new HashSet<String>();
+	}
+
+	public void reset()
+	{
+		this.classExpressionIndex = 0;
+		this.ces.clear();
+		this.owlClassExpression2CEIDMap.clear();
+		this.convertedClassExpressionIDs.clear();
+	}
+
+	public Set<CE> getCEs()
+	{
+		return this.ces;
+	}
+
+	private String getCEID(OWLClassExpression classExpression)
+	{
+		if (this.owlClassExpression2CEIDMap.containsKey(classExpression))
+			return this.owlClassExpression2CEIDMap.get(classExpression);
+		else {
+			String classExpressionID = "CEID" + this.classExpressionIndex++;
+			this.owlClassExpression2CEIDMap.put(classExpression, classExpressionID);
+			return classExpressionID;
+		}
+	}
+}
