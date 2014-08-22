9ed409a03baac844b6b7acd4f2f1bf56bb0df1a7
martin <martin@zippyrate.com>
2014-08-22 16:59:45 -0700

    Some reasoner work

--- src/main/java/org/swrlapi/drools/reasoner/DefaultDroolsOWLReasoner.java
+++ src/main/java/org/swrlapi/drools/reasoner/DefaultDroolsOWLReasoner.java
@@ -1,7 +1,9 @@
 package org.swrlapi.drools.reasoner;
 
+import org.semanticweb.owlapi.vocab.OWLRDFVocabulary;
 import org.swrlapi.drools.owl.axioms.A;
 import org.swrlapi.drools.owl.classexpressions.CE;
+import org.swrlapi.drools.owl.core.C;
 import org.swrlapi.drools.owl.core.I;
 import org.swrlapi.drools.owl.core.L;
 import org.swrlapi.drools.owl.properties.DP;
@@ -50,42 +52,66 @@ public class DefaultDroolsOWLReasoner implements DroolsOWLReasoner
 
 	@Override public CNode getUnsatisfiableClasses()
 	{
-		return null; // TODO
+		return getBottomClassNode();
 	}
 
 	@Override public CNode getTopClassNode()
 	{
-		return null; // TODO
+		CNode cNode = new CNode();
+
+		for (String classID : this.droolsOWLAxiomHandler.getEquivalentClasses(OWLRDFVocabulary.OWL_THING.getShortForm())) {
+			C c = resolveOWLClass(classID);
+			cNode.add(c);
+		}
+		return cNode;
 	}
 
 	@Override public CNode getBottomClassNode()
 	{
-		return null; // TODO
+		CNode cNode = new CNode();
+
+		for (String classID : this.droolsOWLAxiomHandler.getEquivalentClasses(OWLRDFVocabulary.OWL_NOTHING.getShortForm())) {
+			C c = resolveOWLClass(classID);
+			cNode.add(c);
+		}
+		return cNode;
 	}
 
 	@Override public boolean isSatisfiable(CE ce)
 	{
-		return false; // TODO
+		return this.droolsOWLAxiomHandler.getEquivalentClasses(OWLRDFVocabulary.OWL_NOTHING.getShortForm()).contains(ce.getceid());
 	}
 
 	@Override public CNodeSet getDisjointClasses(CE ce)
 	{
+		String classID = ce.getceid();
+
 		return null; // TODO
 	}
 
 	@Override public CNodeSet getSubClasses(CE ce, boolean direct)
 	{
+		String classID = ce.getceid();
+
 		return null; // TODO
 	}
 
 	@Override public CNodeSet getSuperClasses(CE ce, boolean direct)
 	{
+		String classID = ce.getceid();
+
 		return null; // TODO
 	}
 
 	@Override public CNode getEquivalentClasses(CE ce)
 	{
-		return null; // TODO
+		CNode cNode = new CNode();
+
+		for (String classID : this.droolsOWLAxiomHandler.getEquivalentClasses(ce.getceid())) {
+			C c = resolveOWLClass(classID);
+			cNode.add(c);
+		}
+		return cNode;
 	}
 
 	@Override public CNodeSet getTypes(I i, boolean direct)
@@ -97,49 +123,67 @@ public class DefaultDroolsOWLReasoner implements DroolsOWLReasoner
 
 	@Override public INodeSet getInstances(CE ce, boolean direct)
 	{
-		return null;
+		return null; // TODO
 	}
 
 	@Override public INodeSet getSameIndividuals(I individual)
 	{
-		return null;
+		return null; // TODO
 	}
 
 	@Override public INodeSet getDifferentIndividuals(I individual)
 	{
-		return null;
+		return null; // TODO
 	}
 
 	// Object properties
 
 	@Override public OPNode getTopObjectPropertyNode()
 	{
-		return null;
+		OPNode opNode = new OPNode();
+
+		for (String propertyID : this.droolsOWLAxiomHandler.getEquivalentClasses(OWLRDFVocabulary.OWL_TOP_OBJECT_PROPERTY.getShortForm())) {
+			OP op = resolveOWLObjectProperty(propertyID);
+			opNode.add(op);
+		}
+		return opNode;
 	}
 
 	@Override public OPNode getBottomObjectPropertyNode()
 	{
-		return null;
+		OPNode opNode = new OPNode();
+
+		for (String propertyID : this.droolsOWLAxiomHandler.getEquivalentClasses(OWLRDFVocabulary.OWL_BOTTOM_OBJECT_PROPERTY.getShortForm())) {
+			OP op = resolveOWLObjectProperty(propertyID);
+			opNode.add(op);
+		}
+		return opNode;
 	}
 
 	@Override public OPNodeSet getSuperObjectProperties(OP op, boolean direct)
 	{
-		return null;
+		return null; // TODO
 	}
 
 	@Override public OPNodeSet getSubObjectProperties(OP op, boolean direct)
 	{
-		return null;
+		return null; // TODO
 	}
 
 	@Override public OPNode getEquivalentObjectProperties(OP op)
 	{
-		return null;
+		OPNode opNode = new OPNode();
+
+		for (String propertyID : this.droolsOWLAxiomHandler.getEquivalentObjectProperties(op.getid())) {
+			OP eop = resolveOWLObjectProperty(propertyID);
+			opNode.add(eop);
+		}
+		return opNode;
 	}
 
 	@Override public OPNodeSet getDisjointObjectProperties(OPE ope)
 	{
-		return null;
+		return null; // TODO
 	}
 
 	@Override public CNodeSet getObjectPropertyDomains(OPE ope, boolean direct)
@@ -149,58 +193,91 @@ public class DefaultDroolsOWLReasoner implements DroolsOWLReasoner
 
 	@Override public CNodeSet getObjectPropertyRanges(OPE ope, boolean direct)
 	{
-		return null;
+		return null; // TODO
 	}
 
 	@Override public INodeSet getObjectPropertyValues(I individual, OPE ope)
 	{
-		return null;
+		return null; // TODO
 	}
 
 	@Override public DroolsNode<OPE> getInverseObjectProperties(OPE ope)
 	{
-		return null;
+		return null; // TODO
 	}
 
 	// Data properties
 
 	@Override public DPNode getTopDataPropertyNode()
 	{
-		return null;
+		DPNode dpNode = new DPNode();
+
+		for (String propertyID : this.droolsOWLAxiomHandler.getEquivalentClasses(OWLRDFVocabulary.OWL_TOP_DATA_PROPERTY.getShortForm())) {
+			DP dp = resolveOWLDataProperty(propertyID);
+			dpNode.add(dp);
+		}
+		return dpNode;
 	}
 
 	@Override public DPNode getBottomDataPropertyNode()
 	{
-		return null;
+		DPNode dpNode = new DPNode();
+
+		for (String propertyID : this.droolsOWLAxiomHandler.getEquivalentClasses(OWLRDFVocabulary.OWL_BOTTOM_DATA_PROPERTY.getShortForm())) {
+			DP dp = resolveOWLDataProperty(propertyID);
+			dpNode.add(dp);
+		}
+		return dpNode;
 	}
 
 	@Override public Set<L> getDataPropertyValues(I individual, DP dp)
 	{
-		return null;
+		return null; // TODO
 	}
 
 	@Override public DPNodeSet getDisjointDataProperties(DPE dpe)
 	{
-		return null;
+		return null;  // TODO
 	}
 
 	@Override public DPNode getEquivalentDataProperties(DP dp)
 	{
-		return null;
+		DPNode dpNode = new DPNode();
+
+		for (String propertyID : this.droolsOWLAxiomHandler.getEquivalentObjectProperties(dp.getid())) {
+			DP edp = resolveOWLDataProperty(propertyID);
+			dpNode.add(edp);
+		}
+		return dpNode;
 	}
 
 	@Override public DPNodeSet getSubDataProperties(DP dp, boolean direct)
 	{
-		return null;
+		return null;  // TODO
 	}
 
 	@Override public DPNodeSet getSuperDataProperties(DP dp, boolean direct)
 	{
-		return null;
+		return null;  // TODO
 	}
 
 	@Override public CNodeSet getDataPropertyDomains(DP dp, boolean direct)
 	{
-		return null;
+		return null;  // TODO
+	}
+
+	private C resolveOWLClass(String classID)
+	{
+		return null; // TODO
+	}
+
+	private OP resolveOWLObjectProperty(String propertyID)
+	{
+		return null; // TODO
+	}
+
+	private DP resolveOWLDataProperty(String propertyID)
+	{
+		return null; // TODO
 	}
 }
