eff4e9b664dfc22d98741cdb733c28e3a1d3d0f5
martinjoconnor <martin.oconnor@stanford.edu>
2015-03-19 16:44:57 -0700

    Some short name/prefix name terminology cleanup

--- src/test/java/org/swrlapi/SWRLCoreTestCase.java
+++ src/test/java/org/swrlapi/SWRLCoreTestCase.java
@@ -121,6 +121,15 @@ public class SWRLCoreTestCase extends SWRLAPITestBase
 		Assert.assertEquals(result.getIndividual("uncle").getShortName(), "p2");
 	}
 
+	@Test
+	public void TestSWRLInvalidShortLiteral() throws SWRLParseException, SQWRLException
+	{
+		declareOWLDataPropertyAssertion("p1", "hasAge", "42", "xsd:short");
+
+		executeSQWRLQuery("q1", "hasAge(p1, \"42x\"^^\"xsd:short\") -> sqwrl:select(p1)");
+	}
+
+	@Test
 	public void TestSWRLShortLiteralMatch() throws SWRLParseException, SQWRLException
 	{
 		declareOWLDataPropertyAssertion("p1", "hasAge", "42", "xsd:short");
