e08b15a93d40d2cc62ce72d0abe39670f3e0fe48
martinjoconnor <martin.oconnor@stanford.edu>
2015-03-20 11:41:14 -0700

    Updated Drools libraries to 6.2.0.Final; code still use Drools 5 API and knowledge-api library provides adapter to Drools 6 implementation

--- src/main/java/org/swrlapi/drools/owl/core/OO.java
+++ src/main/java/org/swrlapi/drools/owl/core/OO.java
@@ -1,11 +1,13 @@
 package org.swrlapi.drools.owl.core;
 
+import java.io.Serializable;
+
 /**
- * This interface represents an OWL object in Drools, which will include named properties such as classes and individuals,
- * plus class expressions, and OWL axioms.
+ * This interface represents an OWL object in Drools, which will include named properties such as classes and
+ * individuals, plus class expressions, and OWL axioms.
  *
  * @see org.semanticweb.owlapi.model.OWLObject
  */
-public interface OO
+public interface OO extends Serializable
 {
 }
